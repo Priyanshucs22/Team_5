f2fe4a89de79952a8bec6ec3c9659ef5f5136a4a
Utkarshagar <nischalagarwal264@gmail.com>
2025-01-29 23:15:41 +0530

    frontend work

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './navbar.css'
+import { assets } from '../../assets/assets'
+
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <img src={assets.logo} alt="" />
+      <ul className='navbar-menu'>
+      <li>home</li>
+      <li>menu</li>
+      <li>mobile-app</li>
+      <li>contact-us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src="{asserts.search_icon}" alt="" />
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar;
