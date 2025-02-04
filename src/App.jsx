42648e97085c94e7732abd9dce00e98fbd9280df
Utkarshagar <nischalagarwal264@gmail.com>
2025-02-04 09:11:12 +0530

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.css';
-import Navbar from './components/Navbar/navbar';
+import Navbar from './components/navbar/navbar';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Cart from './pages/Cart/Cart';
