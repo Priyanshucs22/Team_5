51115de50f06d7e97518ca4f9eb576a58e111439
Utkarshagar <nischalagarwal264@gmail.com>
2025-02-04 10:32:58 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
         <Route path='/' element={<Home />} />
         <Route path='/cart' element={<Cart />} />
         <Route path='/order' element={<PlaceOrder />} />
+        {/* new update */}
       </Routes>
     </div>
   );
